3a487a43806fd0d7a0cab50d7f0433daa9121813
wbond <will@wbond.net>
2015-10-07 12:08:07 -0400

    Flake8 fixes for setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import setup, find_packages, Command
 import asn1crypto
 
 
-
 class CleanCommand(Command):
     user_options = []
 
@@ -35,7 +34,10 @@ setup(
     name='asn1crypto',
     version=asn1crypto.__version__,
 
-    description='Fast ASN.1 parser and serializer with definitions for private keys, public keys, certificates, CRL, OCSP, CMS, PKCS#7, PKCS#8, PKCS#12, PKCS#5, X.509 and TSA',
+    description=(
+        'Fast ASN.1 parser and serializer with definitions for private keys, public keys, '
+        'certificates, CRL, OCSP, CMS, PKCS#7, PKCS#8, PKCS#12, PKCS#5, X.509 and TSA'
+    ),
     long_description='Docs for this project are maintained at https://github.com/wbond/asn1crypto#readme.',
 
     url='https://github.com/wbond/asn1crypto',
