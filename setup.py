bc3f0f6039ca3447d883d41b4ac955aac72139be
wbond <will@wbond.net>
2015-10-20 01:07:32 -0400

    Added some pypi trove classifiers

--- setup.py
+++ setup.py
@@ -56,8 +56,13 @@ setup(
 
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+
+        'Topic :: Security :: Cryptography',
     ],
 
     keywords='asn1 crypto pki',
