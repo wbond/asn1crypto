fb1f98d67b7cb6ebc8bd33baa478997d5040f911
wbond <will@wbond.net>
2017-01-23 12:18:18 -0500

    Add algos.DSASignature

--- asn1crypto/algos.py
+++ asn1crypto/algos.py
@@ -7,6 +7,7 @@ key cryptography. Exports the following items:
  - AlgorithmIdentifier()
  - DigestAlgorithm()
  - DigestInfo()
+ - DSASignature()
  - EncryptionAlgorithm()
  - HmacAlgorithm()
  - KdfAlgorithm()
@@ -19,6 +20,8 @@ Other type classes are defined that help compose the types listed above.
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 from ._errors import unwrap
+from ._int import fill_width
+from .util import int_from_bytes, int_to_bytes
 from .core import (
     Any,
     Choice,
@@ -508,6 +511,57 @@ class RSAESOAEPParams(Sequence):
             }
         ),
     ]
+
+
+class DSASignature(Sequence):
+    """
+    An ASN.1 class for translating between the OS crypto library's
+    representation of an (EC)DSA signature and the ASN.1 structure that is part
+    of various RFCs.
+
+    Original Name: DSS-Sig-Value
+    Source: https://tools.ietf.org/html/rfc3279#section-2.2.2
+    """
+
+    _fields = [
+        ('r', Integer),
+        ('s', Integer),
+    ]
+
+    @classmethod
+    def from_p1363(cls, data):
+        """
+        Reads a signature from a byte string encoding accordint to IEEE P1363,
+        which is used by Microsoft's BCryptSignHash() function.
+
+        :param data:
+            A byte string from BCryptSignHash()
+
+        :return:
+            A DSASignature object
+        """
+
+        r = int_from_bytes(data[0:len(data) // 2])
+        s = int_from_bytes(data[len(data) // 2:])
+        return cls({'r': r, 's': s})
+
+    def to_p1363(self):
+        """
+        Dumps a signature to a byte string compatible with Microsoft's
+        BCryptVerifySignature() function.
+
+        :return:
+            A byte string compatible with BCryptVerifySignature()
+        """
+
+        r_bytes = int_to_bytes(self['r'].native)
+        s_bytes = int_to_bytes(self['s'].native)
+
+        int_byte_length = max(len(r_bytes), len(s_bytes))
+        r_bytes = fill_width(r_bytes, int_byte_length)
+        s_bytes = fill_width(s_bytes, int_byte_length)
+
+        return r_bytes + s_bytes
 
 
 class EncryptionAlgorithmId(ObjectIdentifier):
