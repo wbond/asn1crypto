7359418c314ed9a5dc9aeeeefeaeea62254aea20
ofek <ofekmeister@gmail.com>
2017-01-25 15:20:55 -0500

    fix signed values

--- asn1crypto/util.py
+++ asn1crypto/util.py
@@ -154,8 +154,8 @@ else:
         """
 
         if width is None:
-            return value.to_bytes((value.bit_length() + 7) // 8,
-                                  byteorder='big', signed=signed)
+            length = (value.bit_length() + 7) // 8 + int(signed)
+            return value.to_bytes(length, byteorder='big', signed=signed)
         else:
             return value.to_bytes(width, byteorder='big', signed=signed)
 
