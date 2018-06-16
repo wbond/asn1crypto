d206112fe8c251419a5e89475db0185628a860f1
Jon Dufresne <jon.dufresne@gmail.com>
2018-06-16 11:02:53 -0700

    Document project as stable and ready for use in production
    Trove classifiers are displayed on PyPI. Helps potential libraries users
    know the state and maturity of the code.
    
    For a full list of trove classifiers, see:
    
    https://pypi.org/pypi?%3Aaction=list_classifiers

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     license='MIT',
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         'Intended Audience :: Developers',
 
